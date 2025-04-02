a138a4a4afbf3caf8268144fd63b58381648dcec
Gavin Hall <gavin-hall@users.noreply.github.com>
2025-04-02 20:35:57 +0000

    button edit

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,7 @@
 import * as Headless from '@headlessui/react'
 import { clsx } from 'clsx'
 import { Link } from './link'
-
+/* test commit comment */
 const variants = {
   primary: clsx(
     'inline-flex items-center justify-center px-4 py-[calc(--spacing(2)-1px)]',
@@ -11,14 +11,14 @@ const variants = {
   ),
   secondary: clsx(
     'relative inline-flex items-center justify-center px-4 py-[calc(--spacing(2)-1px)]',
-    'rounded-full border border-transparent bg-white/15 ring-1 shadow-md ring-[#D15052]/15',
+    'rounded-full border border-transparent bg-white/15 shadow-md ring-1 ring-[#D15052]/15',
     'after:absolute after:inset-0 after:rounded-full after:shadow-[inset_0_0_2px_1px_#ffffff4d]',
     'text-base font-medium whitespace-nowrap text-gray-950',
     'data-disabled:bg-white/15 data-disabled:opacity-40 data-hover:bg-white/20',
   ),
   outline: clsx(
     'inline-flex items-center justify-center px-2 py-[calc(--spacing(1.5)-1px)]',
-    'rounded-lg border border-transparent ring-1 shadow-sm ring-black/10',
+    'rounded-lg border border-transparent shadow-sm ring-1 ring-black/10',
     'text-sm font-medium whitespace-nowrap text-gray-950',
     'data-disabled:bg-transparent data-disabled:opacity-40 data-hover:bg-gray-50',
   ),
